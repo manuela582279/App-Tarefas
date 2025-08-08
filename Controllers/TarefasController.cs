2aa94e4f213e633487b8302cf98995b28691cbb3
mManuela582279 <manuela.domingues@aluno.senai.br>
2025-08-08 16:42:25 -0300

    Adiciona funcionalidade de lista de tarefas
    - Atualiza `_Layout.cshtml`:
      - Altera idioma para `pt-br` e ajusta título para `appTarefas`.
      - Renomeia arquivo de estilo para `appTarefas.styles.css`.
      - Adiciona link no menu para "Lista de Tarefas".
      - Ajusta rodapé para ser fixado na parte inferior.
      - Reorganiza código para melhorar legibilidade.
    - Cria `TarefasController` com ação `Index`.
    - Adiciona modelo `Tarefa` com propriedades `TarefaId`, `Titulo`,
      `Descricao` e `Concluida`.
    - Cria visualização `Index.cshtml` com título "Tarefas" e cabeçalho
      "Lista de Tarefas".

---
+++ Controllers/TarefasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App_Tarefas.Controllers
+{
+    public class TarefasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace App_Tarefas.Models
+{
+    public class Tarefa
+    {
+        //Nome da chave primaria deve ser Nome da classe + "Id"
+        public int TarefaId { get; set; } //Id é a chave primaria
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+
+    }
+}

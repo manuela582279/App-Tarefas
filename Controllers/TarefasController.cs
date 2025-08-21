fa8e616f3cb60f56c77edad4a233e36176458125
mManuela582279 <manuela.domingues@aluno.senai.br>
2025-08-21 14:05:12 -0300

    Implementação de CRUD para tarefas no projeto
    Adicionada a classe `Tarefa` ao namespace `App_Tarefas.Models` com uma lista estática para armazenar tarefas em memória e um contador estático para IDs.
    
    Implementados métodos no `TarefasController` para gerenciar tarefas: `Index`, `Create`, `Edit`, `Details` e `Delete`.
    
    Atualizado o arquivo `Index.cshtml` para exibir a lista de tarefas em uma tabela. Adicionados os arquivos `Create.cshtml`, `Delete.cshtml`, `Details.cshtml` e `Edit.cshtml` para suportar as operações de criação, exclusão, visualização e edição de tarefas.

--- Controllers/TarefasController.cs
+++ Controllers/TarefasController.cs
@@ -1,12 +1,92 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App_Tarefas.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace App_Tarefas.Controllers
 {
     public class TarefasController : Controller
     {
+        //Lista em memoria(grava as informações apenas quando a apliação esta rodando)
+        private static List<Tarefa> _tarefas = new List<Tarefa>();
+        private static int _proximoId = 1;
+        //GET: Tarefas
         public IActionResult Index()
+        {
+            return View(_tarefas);// Envia a lista de tarefas como parametro para a pagina Index.
+        }
+        //GET: Tarefas/Criar
+        //GET -> Metodo para "pegar" a pagina e exibir
+        public IActionResult Create()
         {
             return View();
         }
+        //POST: Tarefas/Criar
+        [HttpPost] //Específica que esse metodo responde a requisições POST
+        [ValidateAntiForgeryToken] //Protege contra ataques 
+        public IActionResult Create(Tarefa tarefa)
+        {
+            if (ModelState.IsValid) //Verifica se os dados enviados são validos
+            {
+                tarefa.TarefaId = _proximoId++;
+                _tarefas.Add(tarefa); //Adiciona a nova tarefa a lista
+                return RedirectToAction("Index"); //Redireciona para a ação Index
+            }
+            return View(tarefa); //Se os dados não forem validos, retorna a mesma view com os dados preenchidos
+        }
+
+        public IActionResult Edit(int id)
+        {
+            //var tarefa = _tarefas[id]; //Trabalhando com lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+
+        }
+
+        //POST: Tarefas/Edit/1
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Tarefa tarefaAtualizada)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+
+            tarefa.Titulo = tarefaAtualizada.Titulo;
+            tarefa.Descricao = tarefaAtualizada.Descricao;
+            tarefa.Concluida = tarefaAtualizada.Concluida;
+
+            return RedirectToAction("Index");
+
+        }
+        //GET: Tarefas/Edit/1
+        public IActionResult Details(int id)
+        {
+            //var tarefa = _tarefas[id]; //Trabalhando com lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+
+
+        }
+        //GET: Tarefas/Edit/1
+        public IActionResult Delete(int id)
+        {
+            //var tarefa = _tarefas[id]; //Trabalhando com lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+
+        }
+        //Post: Tarefas/Delete/1
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefa != null)
+            {
+                _tarefas.Remove(tarefa); //Remove a tarefa da lista
+            }
+            return RedirectToAction("Index");
+
+
+        }
     }
 }
+
